6cd3c8e40c29a5ed4719ceeb0464b52aa36508b2
Maria <sheyko.mari@mail.ru>
2021-03-21 11:19:47 +0300

    feat:done task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,7 +4,13 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(str) {
-  if (typeof (str) !== String) {
-
+  if (typeof (str) == ('string')) {
+    let num = Number(str)
+    if ((typeof (num) !== 'number') || (num >= 15) || (num <= 0) || isNaN(num)) {
+      return false
+    } else {
+      result = (Math.log(15 / num) / (0.693 / 5730));
+      return Math.ceil(result)
+    }
   } else { return false }
 };
