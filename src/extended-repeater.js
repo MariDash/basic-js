0953f61a5ea8ed3b0b02827bf50c884334298b0e
Maria <sheyko.mari@mail.ru>
2021-03-28 21:35:53 +0300

    feat:task done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  str = String(str);
+
+  if (typeof (options.repeatTimes) == 'undefined') {
+    options.repeatTimes = 1;
+  }
+
+  if (!options.hasOwnProperty('separator')) {
+    options.separator = '+'
+  }
+
+  if (!options.hasOwnProperty('addition')) {
+    options.addition = '';
+  }
+  String(options.addition);
+
+  if (typeof (options.additionRepeatTimes) == 'undefined') {
+    options.additionRepeatTimes = 1;
+  }
+
+  if (!options.hasOwnProperty("additionSeparator")) {
+    options.additionSeparator = "|";
+  }
+
+  let result = ''
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str;
+    for (let y = 0; y < options.additionRepeatTimes; y++) {
+      result += options.addition;
+      if (y < options.additionRepeatTimes - 1) {
+        result += options.additionSeparator;
+      }
+    }
+    if (i < options.repeatTimes - 1) {
+      result += options.separator;
+    }
+  }
+  return result;
+}
