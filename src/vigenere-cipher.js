886ef82076e84ea3c73dce05355ea0b86e3402f5
Maria <sheyko.mari@mail.ru>
2021-03-29 16:52:41 +0300

    feat: task done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,16 +1,71 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    if (message == undefined || key == undefined) {
-      throw new Error('Error');
-    }
+  constructor(type = true) {
+    this.type = type
   }
-  decrypt() {
+
+
+  encrypt(message, key) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    if (message == undefined || key == undefined) {
+      throw new Error('Error')
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let mPos
+    let kPos
+    let counter = 0
+    let result = []
+
+    for (let i = 0; i < message.length; i++) {
+      mPos = alphabet.indexOf(message[i]);
+
+      if (mPos === -1) {
+        result.push(message[i])
+        continue;
+      }
+
+      kPos = alphabet.indexOf(key[counter % key.length]);
+      result.push(alphabet[(mPos + kPos) % 26]);
+      counter++;
+    }
+    return this.type ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
     if (encryptedMessage == undefined || key == undefined) {
       throw new Error('Error');
     }
+    //encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let mPos
+    let kPos
+    let counter = 0
+    let result = []
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      mPos = alphabet.indexOf(encryptedMessage[i])
+
+      if (mPos === -1) {
+        result.push(encryptedMessage[i])
+        continue;
+      }
+
+      kPos = alphabet.indexOf(key[counter % key.length]);
+      result.push(alphabet[(mPos - kPos + 26) % 26]);
+      counter++;
+    }
+
+    return this.type ? result.join("") : result.reverse().join("");
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
