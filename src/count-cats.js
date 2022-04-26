35b8020f2466d64e3dcb697765c9541df0fc0ca6
Maria <sheyko.mari@mail.ru>
2022-04-26 21:37:17 +0300

    feat: task done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let arr = [];
+  let i = 0;
+  while (i < matrix.length) {
+    for (let y = 0; y < matrix[i].length; y++) {
+      arr.push(matrix[i][y]);
+    }
+    i++;
+  }
+
+  let result = [];
+  arr.filter(function (item, i) {
+    if (item === "^^") {
+      result.push(arr[i]);
+    }
+  });
+
+  return result.length;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
