b6e14f334121c4b22b4925187a90d7a8e516ce94
Maria <sheyko.mari@mail.ru>
2022-04-26 21:45:16 +0300

    feat: task done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+
+  if (typeof options.repeatTimes == "undefined") {
+    options.repeatTimes = 1;
+  }
+
+  if (!options.hasOwnProperty("separator")) {
+    options.separator = "+";
+  }
+
+  if (!options.hasOwnProperty("addition")) {
+    options.addition = "";
+  }
+  String(options.addition);
+
+  if (typeof options.additionRepeatTimes == "undefined") {
+    options.additionRepeatTimes = 1;
+  }
+
+  if (!options.hasOwnProperty("additionSeparator")) {
+    options.additionSeparator = "|";
+  }
+
+  let result = "";
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str;
+    for (let y = 0; y < options.additionRepeatTimes; y++) {
+      result += options.addition;
+      if (y < options.additionRepeatTimes - 1) {
+        result += options.additionSeparator;
+      }
+    }
+    if (i < options.repeatTimes - 1) {
+      result += options.separator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
