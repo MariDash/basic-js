d3505b5c1a54e27110db579c8e2b0e4575e0b8db
Maria <sheyko.mari@mail.ru>
2022-04-29 20:54:57 +0300

    feat: task done!

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -15,16 +15,16 @@ const chainMaker = {
   },
   removeLink(position) {
     console.log("position", position);
-    if (position > 0) {
-      this.arr.splice(position - 1, 1);
-      return this;
-    } else if (
+    if (
       position <= 0 ||
       typeof position !== "number" ||
       this.arr.length < position
     ) {
-      // this.arr.length = 0;
+      this.arr = [];
       throw new Error("You can't remove incorrect link!");
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
     }
   },
   reverseChain() {
@@ -32,16 +32,9 @@ const chainMaker = {
     return this;
   },
   finishChain() {
-    let Array = [...this.arr];
-    this.arr.length = 0;
-    return Array.join("~~");
-    if (position > 0) {
-      this.arr.splice(position - 1, 1);
-      return this;
-    } else {
-      this.arr.length = 0;
-      throw new Error(`You can't remove incorrect link!`);
-    }
+    let result = [...this.arr];
+    this.arr = [];
+    return result.join("~~");
   },
 };
 
